3ffbd267c41ad94efa68157d9fa42513d70146a5
Francesco Antonio Barletta <franchebarletta@gmail.com>
2025-12-21 20:57:32 -0300

    habitacion de prueba

--- rogue.c
+++ rogue.c
@@ -1,8 +1,26 @@
 #include <ncurses.h>
 
+struct Room{
+    int width;
+    int height;
+};
+
+void draw_room(int x, int y, struct Room r){
+    for (int i = y; i < r.height; i++){
+        for(int j = x; j < r.width; j++){
+           mvaddch(i, j, '.');
+        }
+    }
+}
+
 int main(){
     initscr();
-    printw("Hello World!!!");
+    int row, col;
+    getmaxyx(stdscr,row,col);
+    struct Room map;
+    map.width = col-10;
+    map.height = row-10;
+    draw_room(10, 10, map);
     refresh();
     getch();
     endwin();
